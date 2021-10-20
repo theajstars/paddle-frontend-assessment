84c0b7b8a649b83a928ebe9dff24152845c82207
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2021-10-20 11:39:21 +0100

    Get started with Paddle

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,9 @@ import Nav from "./Nav";
 
 import JumboImage from "../assets/img/top-jumbo-bg.jpg";
 import FeatureSectionImage from "../assets/img/feature-section-background.jpg";
+import PlanImage1 from "../assets/img/pricing_images/pricing1.jpg";
+import PlanImage2 from "../assets/img/pricing_images/pricing2.jpg";
+import PlanImage3 from "../assets/img/pricing_images/pricing3.jpg";
 
 export default function Home() {
   return (
@@ -88,7 +91,95 @@ export default function Home() {
             cheerfully
           </p>
 
-          <div className="plans flex-row"></div>
+          <div className="plans flex-row">
+            <div className="plan flex-column">
+              <div className="flex-column" style={{ alignItems: "center" }}>
+                <img src={PlanImage1} alt="" className="plan-image" />
+                <br />
+                <span className="plan-head">Free Plan</span>
+              </div>
+              <ul className="plan-items">
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+              </ul>
+
+              <div className="flex-column" style={{ alignItems: "center" }}>
+                <span className="jumbo-detail-value">Free</span>
+                <br />
+                <button className="rubik plan-button flex-row">Select</button>
+              </div>
+            </div>
+
+            <div className="plan flex-column">
+              <div className="flex-column" style={{ alignItems: "center" }}>
+                <img src={PlanImage2} alt="" className="plan-image" />
+                <br />
+                <span className="plan-head">Standard Plan</span>
+              </div>
+              <ul className="plan-items">
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+              </ul>
+
+              <div className="flex-column" style={{ alignItems: "center" }}>
+                <span className="jumbo-detail-value">
+                  $9{" "}
+                  <span style={{ fontWeight: 400 }} className="text-dark">
+                    {" "}
+                    / mo
+                  </span>
+                </span>
+                <br />
+                <button className="rubik plan-button flex-row">Select</button>
+              </div>
+            </div>
+
+            <div className="plan flex-column">
+              <div className="flex-column" style={{ alignItems: "center" }}>
+                <img src={PlanImage3} alt="" className="plan-image" />
+                <br />
+                <span className="plan-head">Premium Plan</span>
+              </div>
+              <ul className="plan-items">
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+                <li className="plan-item text-dark">Mauris sem neque</li>
+              </ul>
+
+              <div className="flex-column" style={{ alignItems: "center" }}>
+                <span className="jumbo-detail-value">
+                  $12{" "}
+                  <span style={{ fontWeight: 400 }} className="text-dark">
+                    {" "}
+                    / mo
+                  </span>
+                </span>
+                <br />
+                <button className="rubik plan-button flex-row">Select</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="start-paddle-section flex-column">
+          <span className="body-head start-paddle-head">
+            Get Started With
+            <br />
+            Paddle Today
+          </span>
+          <p className="text-dark choose-plan-text">
+            Praesent mollis lobortis nisl nec laoreet. Duis euismod nisl nec
+            ante <br /> egestas ullamcorper
+          </p>
+          <button className="start-today rubik">Start Today</button>
         </div>
       </Container>
     </>
